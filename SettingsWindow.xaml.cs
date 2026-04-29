5b767dd638cc45edfea73c73fb141ac9b8823eca
abaye <8140hp@gmail.com>
2026-04-29 11:25:23 +0300

    v1.2.0

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -234,11 +234,11 @@ namespace Kolsites
                 var v = typeof(SettingsWindow).Assembly.GetName().Version;
                 AboutVersionText.Text = v != null
                     ? $"גרסה {v.Major}.{v.Minor}.{v.Build}"
-                    : "גרסה 1.1.0";
+                    : "גרסה 1.2.0";
             }
             catch
             {
-                AboutVersionText.Text = "גרסה 1.1.0";
+                AboutVersionText.Text = "גרסה 1.2.0";
             }
 
             SettingsPathText.Text = $"קובץ הגדרות: {SettingsManager.GetSettingsPath()}";
